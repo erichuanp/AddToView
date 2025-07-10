c3272bd1a7bf4b80378094541b5b658faba772f9
erichuanp <erichuanp@gmail.com>
2025-07-11 03:18:46 +0800

    code update

--- Login.py
+++ Login.py
@@ -5,15 +5,18 @@ import cv2
 import os
 import re
 
-url1 = 'http://passport.bilibili.com/qrcode/getLoginUrl'
-url2 = 'http://passport.bilibili.com/qrcode/getLoginInfo'
+url1 = 'https://passport.bilibili.com/x/passport-login/web/qrcode/generate'
+url2 = 'https://passport.bilibili.com/x/passport-login/web/qrcode/poll'
 url3 = 'https://api.bilibili.com/x/web-interface/nav'
 
 
 def login_code():
-    url_json = requests.get(url1).json()
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
+    }
+    url_json = requests.get(url1, headers=headers).json()
     qr_url = url_json['data']['url']
-    key = url_json['data']['oauthKey']
+    key = url_json['data']['qrcode_key']
 
     myqr.run(words=qr_url, colorized=True, save_name=f"{key}.jpg")
 
@@ -21,9 +24,9 @@ def login_code():
     cv2.imshow(key, img_1)
     cv2.waitKey()
 
-    data1 = {"oauthKey": key}
-    r1 = requests.post(url2, data=data1).json()
-
+    data1 = {"qrcode_key": key}
+    r1 = requests.get(url2, data=data1, headers=headers).json()
+    print(r1)
     try:
         text1 = r1['data']['url'].replace('https://passport.biligame.com/crossDomain?', '')
         text2 = re.split("&", text1)
@@ -34,7 +37,7 @@ def login_code():
             if text4[0] == "gourl":
                 continue
             cookie[text4[0]] = text4[1]
-        user_json = requests.get(url3, cookies=cookie).json()
+        user_json = requests.get(url3, cookies=cookie, headers=headers).json()
         uid = user_json['data']['mid']
 
         with open(f"{uid}_cookie.json", "w", encoding='utf-8') as f:
